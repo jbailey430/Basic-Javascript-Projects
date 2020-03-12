78227ddc92c246ad1474ffd116e5568fb2eeb70e
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-03-12 13:22:38 -0700

    commit

--- JS/Basic_JavaScript_1.js
+++ JS/Basic_JavaScript_1.js
@@ -1,3 +1,40 @@
-document.write ("Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\
+
+var B ="this is a Alert"
+
+window.alert(B)
+function myFunction() {
+    var str1 = "Hello";
+    var str2 = "World!";
+    var res = str1.concat(str2);
+    document.getElementById("demo").innerHTML = res;
+}
+
+document.write("Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\" <br> \
 \"Eat my shorts!\" Bart responded.");
+
+document.write("<br>");
+document.write("<br>");
+
+document.write("\"Be who you are and say what you feel,"
++ "because those who mind don\'t matter and those who matter don\'t mind. \""
++ "-Dr. Seuss");
+
+document.write("<br>");
+document.write("<br>");
+
+var Family = "The Baileys", Dad ="Raymond", Mom="Karen",
+    Daughter="Kristen", Son="Tyler";
+var Family = Family.fontcolor("blue");
+var Dad = Dad.fontcolor("blue");
+var Mom =Mom.fontcolor("red");
+var Daughter = Daughter.fontcolor("green");
+var Son = Son.fontcolor("blue");
+
+document.write(Daughter);
+
+document.write("<br>");
+document.write("<br>");
+
+document.write(10 +10);
+
 
