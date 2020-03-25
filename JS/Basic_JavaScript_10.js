4260e5c5fa74367cbe91e082cae540120988a4cb
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-03-25 10:12:13 -0700

    commit

--- JS/Basic_JavaScript_10.js
+++ JS/Basic_JavaScript_10.js
@@ -33,3 +33,77 @@ function array_Function() {
     document.getElementById("Array").innerHTML = "In this picture, the dog is " +
     Dog_Picture[2] + ".";
 }
+
+function constant_function () {
+    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = '$900';
+    document.getElementById("Constant").innerHTML = "The cost of the " +
+        Musical_Instrument.type + " was " + Musical_Instrument.price;
+
+
+
+const X = 10
+X = 2;
+document.write(X);
+
+var B = 10;
+B= 7;
+document.write(B);
+}
+document.write("<br>");
+document.write("<br>");
+
+var X = 82;
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+document.write("<br>");
+document.write("<br>");
+
+var X = 82;
+document.write(X);
+{
+    var X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+document.write("<br>");
+document.write("<br>");
+
+function return_Function () {
+    return Math.PI ;
+}
+document.getElementById("return").innerHTML =return_Function () ;
+
+let car = {
+    make: "Dodge ",
+    module: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.module;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description() ;
+
+var text = "";
+var i ;
+for ( i = 0; i < 10; i++) {
+    if ( i === 3) { break; }
+    text += "The number is " + i + "<br>";
+}
+document.getElementById("break").innerHTML = text;
+
+    var text = "";
+    var i;
+    for (i = 0; i < 10; i++) {
+        if (i === 3) { continue; }
+        text += "The number is " + i + "<br>";
+    }
+    document.getElementById("continue").innerHTML = text;
